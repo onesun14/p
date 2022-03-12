cbf45b266722e093ba053a99195ea55b852263d8
onesun14 <88918794+onesun14@users.noreply.github.com>
2022-03-12 13:32:41 +0900

    Update ph.py

--- ph.py
+++ ph.py
@@ -1,24 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
-
+import streamlit as st
+number = st.number_input('test',1)
 url = 'https://pokemonkorea.co.kr/pokedex/view/{}'
-response = requests.get(url.format(input()))
+response = requests.get(url.format(number))
 
 if response.status_code == 200:
     print("Success")
     source = response.text
     soup = BeautifulSoup(source, 'html.parser')
-    print("soup", soup)
-    # a = soup.find_all("div", {'class':'bx-txt'})
-    # p = soup.find_all(attrs={'class':'bx-txt'})
-    # t = soup.find_all(attrs={'class':'badge badge-quad badge-grass'})
     a = soup.find_all("div", {'class':'col-lg-6 col-12'})
-    print(a)
-    # count = 0
-    # #print("soup hi", soup.h3)
-    # print(t)
-    index = 1
     for i in a:
-        print(i.text.strip())
-        #print(index, i.text.strip())
-        #index += 1
+        abc = i.text.strip()
+        ab = abc.split("\n")
+    for i in range(len(ab)):
+        if ab[i] != '':
+            print(ab[i])
+st.write('test')
+if st.button('test'):
+    for i in range(len(ab)):
+        if ab[i] != '':
+
+            st.write(ab[i])
